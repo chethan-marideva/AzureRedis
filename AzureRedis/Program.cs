2074d8ab623475d02eb25c731fe96a89b666aabd
Chethan Marideva <chethan.marideva@outlook.com>
2019-07-09 15:35:50 +0530

    Add project files.

---
+++ AzureRedis/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AzureRedis
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+
+            string connectionString = "<>";
+
+            using ( var cache = ConnectionMultiplexer.Connect(connectionString)) {
+
+                IDatabase db = cache.GetDatabase();
+
+                bool setValue = db.StringSet("test:key", "some value");
+                Console.WriteLine($"SET: {setValue}");
+
+                string getValue = db.StringGet("test:key");
+                Console.WriteLine($"GET: {getValue}");
+
+                setValue = await db.StringSetAsync("test", "100");
+                Console.WriteLine($"SET: {setValue}");
+
+                getValue = await db.StringGetAsync("test");
+                Console.WriteLine($"GET: {getValue}");
+
+                var result = await db.ExecuteAsync("ping");
+                Console.WriteLine($"PING = {result.Type} : {result}");
+
+                result = await db.ExecuteAsync("flushdb");
+                Console.WriteLine($"FLUSHDB = {result.Type} : {result}");
+
+            }
+
+        }
+    }
+}
